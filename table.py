a6fc010e9ce3d69f2541901ae61a1d2f797aa4d9
Schoen Cho <fourthbus@gmail.com>
2020-04-19 13:05:59 +0100

    update

--- table.py
+++ table.py
@@ -1,27 +1,48 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
+def table_read(ta,tc,th):
+    import pandas as pd
+    import numpy as np
+    from scipy.interpolate import RegularGridInterpolator
 
-This is a temporary script file.
-"""
+    file = r'data short.xlsx'
+    lt = pd.read_excel(file)
+    
+    # ta = 293.
+    # tc = 290.
+    # th = 400.
+    
+    ahx_interest = []
+    chx_interest = []
+    hhx_interest = []
+    
+    # AHX
+    ahx_list     = lt['AHX'] # locate the AHX range and values
+    ahx_list     = ahx_list.drop_duplicates(keep='first')
+    ahx_interest = ahx_list.iloc[(ahx_list-ta).abs().argsort()[:2]].sort_index().values.tolist() # find the two closest AHX values  
+    rows         = lt[lt['AHX'].isin(ahx_interest)]
+    
+    # CHX
+    chx_list     = lt['CHX'] # locate the AHX range and values
+    chx_list     = chx_list.drop_duplicates(keep='first')
+    chx_interest = chx_list.iloc[(chx_list-tc).abs().argsort()[:2]].sort_index().values.tolist()
+    rows         = rows[rows['CHX'].isin(chx_interest)]
+    
+    # HHX
+    hhx_list     = lt['HHX'] # locate the AHX range and values
+    hhx_list     = hhx_list.drop_duplicates(keep='first')
+    hhx_interest = hhx_list.iloc[(hhx_list-th).abs().argsort()[:2]].sort_index().values.tolist()
+    rows         = rows[rows['HHX'].isin(hhx_interest)]
+    
+    # transform the 2x2x2 matrix to a interpolatble format qh_mat(ta,tc,th)
+    qh_mat = np.zeros((len(ahx_interest),len(chx_interest),len(hhx_interest)))
+    qc_mat = np.zeros((len(ahx_interest),len(chx_interest),len(hhx_interest)))
+    
+    for i in range(len(ahx_interest)):
+        for j in range(len(chx_interest)):
+            for k in range(len(hhx_interest)):
+                qh_mat[i,j,k] = rows[(rows['AHX']==ahx_interest[i]) & (rows['CHX']==chx_interest[j]) & (rows['HHX']==hhx_interest[k])]['Q_HHX'].to_numpy()
+                qc_mat[i,j,k] = rows[(rows['AHX']==ahx_interest[i]) & (rows['CHX']==chx_interest[j]) & (rows['HHX']==hhx_interest[k])]['Q_CHX'].to_numpy()
+    
+    qh = RegularGridInterpolator((ahx_interest,chx_interest,hhx_interest), qh_mat)(np.array([ta,tc,th]))[0]
+    qc = RegularGridInterpolator((ahx_interest,chx_interest,hhx_interest), qc_mat)(np.array([ta,tc,th]))[0]
 
-from pandas import DataFrame, read_csv
-import matplotlib.pyplot as plt
-import pandas as pd
-
-
-file = r'data short.xlsx'
-lt = pd.read_excel(file)
-#print(lt['AHX'])
-
-#ta = float(input('AHX T = '))
-#tc = float(input('CHX T = '))
-#th = float(input('HHX T = '))
-ta = 295.
-tc = 256.
-th = 405.
-
-ahx_list = lt['AHX'] # locate the AHX range and values
-ahx_list.drop_duplicates(keep='first',inplace=True)
-ahx_interest = ahx_list.iloc[(ahx_list-ta).abs().argsort()[:2]].values.tolist() # find the two closest AHX values
-
+    return [qc,qh]
