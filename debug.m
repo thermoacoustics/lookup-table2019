288fec50ba31a479f6fa05478c2f674d6cb8d990
Schoen Cho <fourthbus@gmail.com>
2020-05-14 01:38:29 +0100

    updates xlsx

--- debug.m
+++ debug.m
@@ -1,6 +1,7 @@
 clear
 close all
-load('table.mat')
+load('table_ch_add.mat')
 [data] = filereader('3sided_tx_run.ip',data);
 % scatter(data(:,2),data(:,3));
-save('table.mat','data')
+% pause
+save('table_ch_add.mat','data')
